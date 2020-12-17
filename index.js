019a87f2105e612d94afbf88e6517dea2d7df9e1
Chris Reed <cr31293@gmail.com>
2020-12-17 17:57:12 -0500

    initial simple server setup

---
+++ index.js
@@ -0,0 +1,76 @@
+const  { ApolloServer , gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Book {
+        title: String
+        author: Author
+    }
+
+    type Author {
+        name: String
+        books: [Book]
+    }
+
+    type Query {
+        books: [Book]
+    }
+    `;
+
+const books = [
+    {
+        title: 'Eragon',
+        author: 'Christopher Paolini',
+    },
+    {
+        title: 'Eldest',
+        author: 'Christopher Paolini',
+    },
+    {
+        title: 'Brisingr',
+        author: 'Christopher Paolini',
+    },
+    {
+        title: 'Inheritance',
+        author: 'Christopher Paolini',
+    },
+    {
+        title: "Harry Potter and the Sorcerer's Stone",
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Harry Potter and the Chamber of Secrets',
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Harry Potter and the Prisoner of Azkaban',
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Harry Potter and the Goblet of Fire',
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Harry Potter and the Order of the Phoenix',
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Harry Potter and the Half-Blood Prince',
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Harry Potter and the Deathly Hallows',
+        author: 'J.K. Rowling',
+    },
+];
+
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
